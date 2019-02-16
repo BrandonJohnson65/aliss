f9a1293d5afde524944471d4af752901a2d274f4
Brandon Johnson <BrandonJohnson659@gmail.com>
2019-02-16 03:30:36 +0000

    send email on comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.delivery_method = :test
+  
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
@@ -7,7 +9,7 @@ Rails.application.configure do
     domain: "example.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name :ENV['GMAIL_ADDRESS'],
+    user_name: ENV['GMAIL_ADDRESS'],
     password: ENV['GMAIL_PASSWORD']
   }
   config.action_mailer.default_url_options = { host: 'localhost:3030'}
