deacf0b223e92167d71cce607c383fe3f14b8954
Brandon Johnson <BrandonJohnson659@gmail.com>
2019-02-22 19:48:59 +0000

    nomster photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,14 @@
-class PhotosController < ApplicationController
+ class PhotosController < ApplicationController
   before_action :authenticate_user
+  
   def create
     @place = Place.find(params[:place_id])
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:picture)
   end
 
   
